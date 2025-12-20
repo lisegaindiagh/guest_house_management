aefd6a8ceed6a6783aaf44508931474653091774
Priyansh Vaghani <priyansh.v@rkitsoftware.com>
2025-12-20 17:55:47 +0530

    common file added

---
+++ lib/Api/api_provider.dart
@@ -0,0 +1,75 @@
+import 'package:dio/dio.dart';
+
+import '../Common/app_common.dart';
+
+/* Api call Method Declared
+* set Base url
+* */
+class ApiProvider {
+  final Dio _dio = Dio();
+
+  String baseUrl = "";
+
+  Future<dynamic> getServerResponse(
+    String url,
+    String method, {
+    var params,
+    var queryParams,
+    bool isTemplate = false,
+    bool isCallCenServer = false,
+  }) async {
+    dynamic response;
+
+    String cUrl = baseUrl + url;
+    Options options = Options(
+      headers: {
+        'Content-Type': 'application/json',
+        'authorization':
+            "Bearer ${await AppCommon.sharePref.getString(AppCommon.sessionKey.token)}",
+      },
+    );
+
+    try {
+      if (method.toLowerCase() == "get") {
+        response = await _dio.get(
+          cUrl,
+          options: options,
+          queryParameters: !AppCommon.isEmpty(queryParams) ? queryParams : {},
+        );
+      } else if (method.toLowerCase() == "post") {
+        response = await _dio.post(
+          cUrl,
+          options: options,
+          data: !AppCommon.isEmpty(params)
+              ? AppCommon.encodeJsonData(params)
+              : {},
+          queryParameters: !AppCommon.isEmpty(queryParams) ? queryParams : {},
+        );
+      } else if (method.toLowerCase() == "put") {
+        response = await _dio.put(
+          cUrl,
+          options: options,
+          data: params,
+          queryParameters: !AppCommon.isEmpty(queryParams) ? queryParams : {},
+        );
+      } else if (method.toLowerCase() == "delete") {
+        response = await _dio.delete(
+          cUrl,
+          options: options,
+          queryParameters: !AppCommon.isEmpty(queryParams) ? queryParams : {},
+        );
+      }
+    } catch (e) {
+      // The exception is of type DioError
+      if (e is DioException) {
+        if (e.response!.statusCode == 401) {}
+        AppCommon.displayToast(e.response!.data["Message"]);
+      }
+    }
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      throw Exception("Failed to Load");
+    }
+  }
+}
---
+++ lib/Common/app_common.dart
@@ -0,0 +1,124 @@
+import 'dart:convert';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'session_key.dart';
+import 'session_manager.dart';
+
+class AppCommon {
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  static SharePref sharePref = SharePref();
+  static SessionKey sessionKey = SessionKey();
+
+  /*
+  * Method is used to listen the internet connectivity changes
+  * This method is called from Build of MyApp class
+  * */
+  static displayNetworkPopup() {
+    Connectivity().onConnectivityChanged.listen((
+      List<ConnectivityResult> result,
+    ) {
+      if (result[0] == ConnectivityResult.none) {
+        AppCommon.offlineDialog();
+      }
+    });
+  }
+
+  /*
+  * This Method is used to show dialog if no-internet
+  * */
+  static Future<dynamic> offlineDialog() async {
+    if (!await AppCommon.isOnline()) {
+      showDialog(
+        context: navigatorKey.currentContext!,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Alert"),
+            content: Text("Please check your internet."),
+            actions: [
+              ElevatedButton(
+                onPressed: () async {
+                  if (await AppCommon.isOnline()) {
+                    Navigator.pop(AppCommon.navigatorKey.currentContext!, true);
+                  }
+                },
+                child: Text("Retry"),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  /*
+  * This Method is used to find internet connectivity in phone
+  * */
+  static Future<bool> isOnline() async {
+    List<ConnectivityResult> isConnected = await Connectivity()
+        .checkConnectivity();
+    if (isConnected[0] == ConnectivityResult.mobile ||
+        isConnected[0] == ConnectivityResult.wifi) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /*
+  * Method is use to check Empty blank or null of the variable
+  * */
+  static bool isEmpty<T>(T value) {
+    if (value == null) {
+      return true;
+    }
+
+    if (value is String && value.trim().isEmpty) {
+      return true;
+    }
+
+    if (value is Iterable && value.isEmpty) {
+      return true;
+    }
+
+    if (value is Map && value.isEmpty) {
+      return true;
+    }
+
+    if (value.toString().trim() == "{}") {
+      return true;
+    }
+
+    if (value.toString().trim() == "null") {
+      return true;
+    }
+
+    return false;
+  }
+
+  /*
+  * Method is use to convert any object to dynamic(Json).
+  * */
+  static dynamic decodeJson(var value) {
+    return jsonDecode(value);
+  }
+
+  /*
+  * Method is use to convert any object to dynamic(Json).
+  * */
+  static dynamic encodeJsonData(var value) {
+    return jsonEncode(value);
+  }
+
+  /*
+    Method is use to displayToast message
+  * Parameters -
+  *             BuildContext - Pass Context of you Screen.
+  *             String - Pass String message witch one Display in Toast
+  */
+  static void displayToast(String message) {
+    Fluttertoast.showToast(msg: message, gravity: ToastGravity.BOTTOM);
+  }
+}
