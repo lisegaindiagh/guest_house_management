6e79e9689844bae6196e28c559576078f77b2a62
Priyansh Vaghani <priyansh.v@rkitsoftware.com>
2025-12-23 17:25:48 +0530

    ScrollConfiguration added

--- lib/Common/colors.dart
+++ lib/Common/colors.dart
@@ -8,6 +8,6 @@ class AppColors {
   Color primaryColor = const Color(0XFF0F4C5C); // primary color
   Color white = const Color(0xFFFFFFFF); // white color
   Color black = const Color(0xFF000000); // black color
-  Color backgroundColor = const Color(0xFFF8F9FA);
+  Color backgroundColor = const Color(0xFFEFEFEF);
   Color btnColor = const Color(0xFFD4AF37);
 }
